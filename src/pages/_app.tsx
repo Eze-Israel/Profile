c204c6652d15e0e101c84273f37e0dbe7f682e37
Eze-Israel <ezeisraeloluoma123@gmail.com>
2024-12-31 10:08:21 +0100

    setup app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/global.css';
+import { AppProps } from 'next/app';
+
+export default function MyApp({
+    Component, pageProps }: AppProps ){
+        return <Component 
+        {...pageProps}
+        />
+    }
+import React from 'react'
+
